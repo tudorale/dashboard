0c53fb6fbc9be7547af89a5a02e4e9421b0c937e
Tudor Alexandru <tudorale2005@gamil.com>
2020-08-28 01:41:23 -1200

    Updates

--- precache-manifest.ecb8d206f79920b41871284eb431fdef.js
+++ precache-manifest.ecb8d206f79920b41871284eb431fdef.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d856ab69279a475a368766759dbb9aa9",
+    "revision": "412ee29b83e33d7665446d1524e5b070",
     "url": "/dashboard/index.html"
   },
   {
